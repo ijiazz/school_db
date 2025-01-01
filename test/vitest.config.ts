438ec175db0719168cfd951ee2b68d8001f81d42
Eaviyi <eavidy@qq.com>
2025-01-01 21:21:59 +0800

    ci: 在容器中运行测试

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -1,7 +1,10 @@
 import type { ViteUserConfig } from "vitest/config";
-
+import process from "node:process";
 import path from "node:path";
 const dirname = import.meta.dirname!;
+
+const PG_URL = process.env.PG_URL || "pg://test@127.0.0.1:5432/postgres";
+
 export default {
   esbuild: { target: "es2022" },
   test: {
@@ -9,7 +12,7 @@ export default {
       { find: /^@ijia\/data\//, replacement: path.join(dirname, "../src") + "/" },
     ],
     env: {
-      TEST_LOGIN_DB: "pg://test@127.0.0.1:5432/postgres",
+      TEST_LOGIN_DB: PG_URL,
       IJIA_TEMPLATE_DBNAME: "test_ijia_template",
     },
     include: ["tests/**/*.test.ts"],
