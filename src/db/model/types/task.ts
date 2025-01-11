3c62128f61fb389c2f19513bf5bced89b7db3ef7
Eaviyi <eavidy@qq.com>
2025-01-11 16:24:40 +0800

    feat: 创建评论任务 添加 commentImage 选项

--- src/db/model/types/task.ts
+++ src/db/model/types/task.ts
@@ -1,3 +1,5 @@
+import type { MediaLevel } from "../tables.ts";
+
 export type SyncUserProfileTask<E = any> = {
   uid: string;
   /** 平台的头像 uri , 用于判断头像是否变化 */
@@ -7,8 +9,9 @@ export type SyncUserProfileTask<E = any> = {
 
 export type SyncAssetListTask<E = any> = {
   uid: string;
-  /** 只读取统计信息 */
+  /** 只读取统计信息。默认情况下获取到作品数据后，如果作品存在资源，会立即添加同步资源的任务 */
   statOnly?: boolean;
+  /** 只获取指定日期之后的作品 */
   afterDate?: Date;
   extra: E;
 };
@@ -21,22 +24,25 @@ export type SaveAssetTask<E = any> = {
   ignoreImage?: boolean;
   ignoreCover?: boolean;
 };
+export type SyncAssetCommentCommon<E = any> = {
+  /** 所属用户id  */
+  uid: string;
+  /** 作品 id */
+  pid: string;
 
-export type SyncAssetCommentTask<E = any> = {
-  /** 作品uid.   */
-  uid: string;
-  pid: string;
-  ignoreReply?: boolean;
-  afterDate?: Date | null;
-  extra: E;
-};
-export type SyncCommentReplyTask<E = any> = {
-  /** 作品uid  */
-  uid: string;
-  pid: string;
-  cid: string;
+  /** 评论图像保存等级。默认为 [MediaLevel.origin,MediaLevel.thumb] */
+  commentImage?: MediaLevel[];
+  /** 只获取指定日期之后的评论 */
   afterDate?: Date;
   extra: E;
+};
+export type SyncAssetCommentTask<E = any> = SyncAssetCommentCommon<E> & {
+  /** 是否忽略评论回复，默认情况下，如果为热门评论（作者赞过或回复过，或者点赞数达到指定值） 则为false, 否则为 true */
+  ignoreReply?: boolean;
+};
+export type SyncCommentReplyTask<E = any> = SyncAssetCommentCommon<E> & {
+  /** 根评论 id */
+  cid: string;
 };
 
 export type CrawlTaskData = {
