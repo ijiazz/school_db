6687e00f88f942fd7e6dc2e11cad6cc822d59450
Eaviyi <eavidy@qq.com>
2025-01-29 14:23:02 +0800

    chore: 补全 ijia_web 角色权限

--- sql/roles/ijia_web.sql
+++ sql/roles/ijia_web.sql
@@ -1,3 +1,15 @@
 SET client_encoding = 'UTF8';
 
 CREATE ROLE ijia_web LOGIN INHERIT;
+
+GRANT SELECT ON user_avatar TO ijia_web;
+GRANT SELECT ON pla_user TO ijia_web;
+GRANT SELECT ON watching_pla_user TO ijia_web;
+
+GRANT SELECT ON pla_asset TO ijia_web;
+GRANT SELECT ON asset_image TO ijia_web;
+GRANT SELECT ON asset_video TO ijia_web;
+GRANT SELECT ON asset_audio TO ijia_web;
+
+GRANT SELECT ON pla_comment TO ijia_web;
+GRANT SELECT ON comment_image TO ijia_web;
