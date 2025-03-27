19c400d64e871a9e494c560b0d3a5e224d5b0f94
Eaviyi <eavidy@qq.com>
2025-03-27 20:52:45 +0800

    fix: 修复 sys.log create_time 的默认值

--- sql/init/tables_system.sql
+++ sql/init/tables_system.sql
@@ -4,7 +4,7 @@ CREATE TABLE sys.log(
     name VARCHAR NOT NULL,
     info JSONB NOT NULL,
     level VARCHAR NOT NULL,
-    create_time TIMESTAMPTZ NOT NULL DEFAULT 'now()'
+    create_time TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 CREATE INDEX idx_sys_log_level_name_create_time ON sys.log(level, name, create_time);
 CREATE INDEX idx_sys_log_level ON sys.log(create_time);
