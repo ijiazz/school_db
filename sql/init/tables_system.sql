d4b94a17d70bdd9c7be0cdbf92a635b26a76e659
Eaviyi <eavidy@qq.com>
2025-04-20 23:56:33 +0800

    update

--- sql/init/tables_system.sql
+++ sql/init/tables_system.sql
@@ -8,3 +8,14 @@ CREATE TABLE sys.log(
 );
 CREATE INDEX idx_sys_log_level_name_create_time ON sys.log(level, name, create_time);
 CREATE INDEX idx_sys_log_level ON sys.log(create_time);
+
+CREATE TABLE sys.review_log(
+    id SERIAL PRIMARY KEY,
+    reviewer_id INT NOT NULL REFERENCES public.user(id) ON DELETE CASCADE, -- 审核者 id
+    operation_time TIMESTAMPTZ NOT NULL DEFAULT now(),  
+    review_result BOOLEAN, -- 审核结果 true 为审核通过，false 为审核不通过
+    target VARCHAR NOT NULL, -- 审核对象
+    target_id VARCHAR NOT NULL -- 审核对象的 id
+);
+CREATE INDEX idx_review_log_target ON review_log(target,target_id,operation_time);
+CREATE INDEX idx_review_log_reviewer_id ON review_log(reviewer_id,operation_time);
