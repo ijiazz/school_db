3bd1dd1462272a58047deca5efa9c7f3b9466070
Eaviyi <eavidy@qq.com>
2026-01-29 14:11:20 +0800

    update

--- sql/init/table/public/examination/question.sql
+++ sql/init/table/public/examination/question.sql
@@ -1,3 +1,9 @@
+CREATE TYPE exam_question_type AS ENUM (
+    'single_choice', -- 单选题
+    'multiple_choice', -- 多选题
+    'true_false' -- 判断题 
+); 
+
 CREATE TABLE exam_question( -- 试题
     id INT NOT NULL PRIMARY KEY REFERENCES post(id) ON DELETE CASCADE ON UPDATE CASCADE,
     user_id INT REFERENCES public.user(id) ON DELETE SET NULL ON UPDATE CASCADE, -- 出题人 id
@@ -5,7 +11,7 @@ CREATE TABLE exam_question( -- 试题
     update_time TIMESTAMPTZ NOT NULL DEFAULT now(), -- 更新时间
     content_text VARCHAR(10000), -- 内容文本
     content_text_struct JSONB, -- 文本扩展信息
-
+    question_type exam_question_type NOT NULL, -- 题目类型
     answer_index INT[] NOT NULL, -- 题目答案
     option_text VARCHAR[], -- 选项文本
 
@@ -30,25 +36,3 @@ CREATE TABLE exam_question_media( -- 试题媒体资源
     type media_type NOT NULL, -- 资源类型
     PRIMARY KEY (question_id, index)
 );
-
-
-CREATE TABLE exam_question_collection( -- 试题收藏夹
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES public.user(id) ON DELETE CASCADE ON UPDATE CASCADE, -- 用户 id
-    name VARCHAR(100) NOT NULL, -- 收藏夹名称
-    description VARCHAR(500), -- 描述
-    question_count INT NOT NULL DEFAULT 0, -- 试题数量
-    create_time TIMESTAMPTZ NOT NULL DEFAULT now(), -- 创建时间
-    update_time TIMESTAMPTZ NOT NULL DEFAULT now() -- 更新时间
-);
-
-CREATE INDEX idx_exam_question_collection_user_list ON exam_question_collection(user_id, name); -- 用户收藏夹列表查询
-
-CREATE TABLE exam_question_collection_item(
-    question_collection INT NOT NULL REFERENCES exam_question_collection(id) ON DELETE CASCADE, -- 收藏夹 id
-    question_id INT NOT NULL REFERENCES exam_question(id) ON DELETE CASCADE, -- 试题 id
-    add_time TIMESTAMPTZ NOT NULL DEFAULT now(), -- 添加时间
-    PRIMARY KEY (question_collection, question_id)
-);
-
-CREATE INDEX idxfk_exam_question_collection_item_question ON exam_question_collection_item(question_id);
